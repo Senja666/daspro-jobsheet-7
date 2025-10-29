82dc873ba202846379098f5185cdda45892529a3
Senja666 <senjabsns@gmail.com>
2025-10-29 12:30:28 +0700

    Square 29

--- Square29.java
+++ Square29.java
@@ -1,1 +1,15 @@
 import java.util.Scanner;
+public class Square29 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukan nilai N = ");
+        int N = sc.nextInt();
+        for(int iOuter = 1 ; iOuter <= N ; iOuter++){
+            for (int i =1; i <= N; i++) {
+                System.out.print("*"); 
+            }
+            System.out.println();
+
+        }
+    }
+}
